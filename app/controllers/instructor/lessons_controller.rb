7dc662b0a3c6c20f8d9121241578fb09265b21f1
Jade Cribbs <jccjdb@aol.com>
2020-02-25 03:21:37 +0000

    L25C

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -22,9 +22,9 @@ class Instructor::LessonsController < ApplicationController
   helper_method :current_section
   def current_section
     @current_section ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video)
-    end
   end
 end
