ba4421fb778397dceb353c1fa806223d6fb438af
Jade Cribbs <jccjdb@aol.com>
2020-02-27 01:57:11 +0000

    L29C

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :users_authorized_to_view_lessons, only: [:show]
+
   def show
   end
 
   private
+
+  def users_authorized_to_view_lessons
+    current_course = current_lesson.section.course
+    if !current_user.enrolled_in?(current_course)      
+      redirect_to course_path(current_course), alert: 'Unauthorized'
+
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
